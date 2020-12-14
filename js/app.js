fa82e4d40984dc78c7c898e5bfab4314cb70a016
abderrahim <info.abderrahimtijani@gmail.com>
2020-12-14 23:31:04 +0100

    use swiper js to create testimonials slider

--- js/app.js
+++ js/app.js
@@ -48,48 +48,22 @@ function clickHandler(e) {
 
 
 
-/* for testimonials from codepen */
+/* for testimonials from swiper */
+var mySwiper = new Swiper('.swiper-container', {
+  // autoplay
+  autoplay: {
+    delay: 5000,
+  },
 
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-	effect: 'coverflow',
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: '2',
-	// coverflowEffect: {
-	//   rotate: 50,
-	//   stretch: 0,
-	//   depth: 100,
-	//   modifier: 1,
-	//   slideShadows : true,
-	// },
-	
-	coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 50,
-        modifier: 6,
-        slideShadows : false,
-	  },
-	  
-  });
-  
-  
-var galleryTop = new Swiper('.swiper-container.testimonial', {
-	speed: 400,
-	spaceBetween: 50,
-	autoplay: {
-	  delay: 3000,
-	  disableOnInteraction: false,
-	},
-	direction: 'vertical',
-	pagination: {
-	  clickable: true,
-	  el: '.swiper-pagination',
-	  type: 'bullets',
-	},
-	thumbs: {
-		swiper: galleryThumbs
-	  }
-  });
-  
-/*  for testimonials from codepen */
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+ 
+   // pagination
+   pagination: {
+     el: '.swiper-pagination',
+   },
+});
+
+
+/*  for testimonials from swiper */
